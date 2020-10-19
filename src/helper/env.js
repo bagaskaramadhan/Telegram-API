5a688a608d56b4738fd38c7ec7d6708debb95f4d
bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-10-19 15:12:21 +0700

    mail and response

--- src/helper/env.js
+++ src/helper/env.js
@@ -5,5 +5,9 @@ module.exports = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASS,
-    DATABASE: process.env.DB_NAME
+    DATABASE: process.env.DB_NAME,
+    SECRETKEY: process.env.SECRET,
+    USEREMAIL: process.env.USEREMAIL,
+    USERPASS: process.env.USERPASS,
+    HOSTURL: process.env.HOSTURL
 }
